28a647d8b23471d38137e12dc12b586b12123354
WON SEOB SEO <pghoou@hotmail.com>
2016-01-17 10:56:32 +0200

    adapting to identity module

--- assets/js/pages/product/ProductsController.js
+++
@@ -1,9 +0,0 @@
-angular.module('ProductsModule').controller('ProductsController', ['$scope', function($scope){
-  $scope.quantity = 0;
-  $scope.addToCart = function(item) {
-    console.log("item", item);
-    simpleCart
-      .add('quantity='+$scope.quantity, 'name='+$scope.name, 'price='+$scope.price);
-    return false;
-  };
-}]);
