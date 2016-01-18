358efdbaff56fa4193db41206b5cec97e087a9fb
WON SEOB SEO <pghoou@hotmail.com>
2016-01-18 16:22:25 +0200

    Merge branch 'master' of https://github.com/wontheone1/web_shop2015-login
    # Conflicts:
    #	tasks/pipeline.js
    #	views/homepage.ejs
    #	views/layout.ejs

--- assets/js/pages/addItem/addItemModule.js
+++ assets/js/pages/addItem/addItemModule.js
@@ -10,10 +10,17 @@ angular.element(document).ready(function () {
 
 angular.module('AddItemModule').controller('AddItemController', ['$scope', '$http', 'toastr',
   function ($scope, $http, toastr) {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = new Date(yyyy+'-'+mm+'-'+dd);
+    console.log(today);
 
     // set-up loading state
     $scope.addItemForm = {
-      loading: false
+      loading: false,
+      manufacturedDate: today
     }
 
     $scope.submitAddItemForm = function () {
