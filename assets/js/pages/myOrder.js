c463f67ea821bc1460fb30e9fdd210433b50221a
WON SEOB SEO <pghoou@hotmail.com>
2016-01-20 01:16:10 +0200

    Merge branch 'master' of https://github.com/wontheone1/web_shop2015-login
    # Conflicts:
    #	tasks/pipeline.js
    #	views/homepage.ejs
    #	views/layout.ejs

--- assets/js/pages/myOrder.js
+++ assets/js/pages/myOrder.js
@@ -3,11 +3,39 @@
  */
 var app = angular.module('myOrderModule', ['SessionModule']);
 
-app.controller('myOrderController', ['$scope', '$http', function($scope, $http) {
+
+app.controller('myOrderController', ['$scope', '$http', function ($scope, $http) {
+
+  // constructing data for My Orders page
+  var orders = [];
+  var orderDetails = [];
+  var presentation = [];
+  var row = [];
+  var items = [];
+
   $http.get('/api/order').then(function (response) {
-    $scope.orders = response.data;
-  });
-  $http.get('/api/orderDetail').then(function (response) {
-    $scope.orderDetails = response.data;
-  });
+      $scope.orders = response.data;
+      orders = response.data;
+      // console.log(orders);
+    })
+    .then(function (response) {
+      $http.get('/api/orderDetail').then(function (response) {
+        $scope.orderDetails = response.data;
+        orderDetails = response.data;
+        // console.log(orderDetails);
+      }).then(function (response) {
+        for (var i = 0; i < orders.length; i++) {
+          for (var j = 0; j < orderDetails.length; j++) {
+            if (orders[i].id === orderDetails[j].id) {
+              items.push(orderDetails[j].item.name);;
+            }
+          };
+          row.push({oid: orders[i].id, items: items});
+          console.log('o' + orders);
+          console.log('od' + orderDetails);
+        };
+      })
+    });
+
+
 }]);
