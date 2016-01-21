7d048f85036fcb462073f7baf4a322958c7f40dd
WON SEOB SEO <pghoou@hotmail.com>
2016-01-21 21:15:23 +0200

    Merge branch 'master' of https://github.com/wontheone1/web_shop2015-login
    # Conflicts:
    #	api/controllers/OrderController.js
    #	api/policies/OrIsMerchant.js

--- api/controllers/OrderDetailController.js
+++ api/controllers/OrderDetailController.js
@@ -6,11 +6,6 @@
  */
 
 module.exports = {
-  //getOrderDetails: function (req, res) {
-  //  OrderDetail.find().exec(function (err, orderDetails){
-  //    if (err) return res.negotiate(err);
-  //    return res.json(200, orderDetails);
-  //  })
-  //}
+
 };
 
